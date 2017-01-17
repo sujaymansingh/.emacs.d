c1976d3fbe17e81f61de56877e820f2833f64bc7
Sujay Mansingh <sujaymansingh@gmail.com>
2017-01-17 09:07:58 +0000

    Add left margin

--- init.el
+++ init.el
@@ -8,3 +8,10 @@
 (require 'evil)
 (evil-mode 1)
 
+
+; Left margin (with line-numbers and a little spacing between the numbers and the text!)
+(global-linum-mode t)
+(setq linum-format "%d ")
+(setq linum-format "%4d \u2502 ")
+(line-number-mode 1)
+
